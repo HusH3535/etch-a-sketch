2a51c0ef76dc5414d4f22d9099a3249dfb36bcf7
Angel Pena <theangel3535@gmail.com>
2023-06-29 14:10:24 -0400

    add ability to re-size the grid and re-structure the code

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
 const container = document.querySelector('.white-board');
 
-const cell_num = 16;
+let cell_num = 16;
 
-let cells = new Array(cell_num);
+let cells;
 
-for (let index = 0; index < cells.length; index++) {
-    cells[index] = new Array(cell_num);
+CreateGrid();
+
+function CreateArrayOfCells() {
+    cells = new Array(cell_num);
+    for (let index = 0; index < cells.length; index++) {
+        cells[index] = new Array(cell_num);
+    }
 }
 
-for (let i = 0; i < cells.length; i++) {
-    const cellArray = document.createElement('div');
-    const cellArrayWidth = (1/cell_num) * 100;
-    cellArray.style.cssText = `flex-basis: ${cellArrayWidth}%; display: flex; flex-growth: 0; flex-shirnk: 0`;
-    container.appendChild(cellArray);
+function ResetGrid() {
+    DeleteCurrentGrid();
+    CreateGrid();
+}
 
+function CreateGrid() {
 
-    for (let j = 0; j < cells[i].length; j++) {
-        const cell = document.createElement('div');
-        
-        cell.classList.add('cell');  
-        const basis = (1/cell_num) * 100;
-        cell.style.cssText = `flex-basis: ${basis}%; flex-growth: 0; flex-shirnk: 0`;
-        cell.addEventListener('click', (e) => {
-            if (e.target.style.background == 'black') {
-                e.target.style.background =  `#f9faf8`;
-            }else{
-                e.target.style.background = 'black';
-            }
+    CreateArrayOfCells()
+
+    for (let i = 0; i < cells.length; i++) {
+
+        const cellArray = document.createElement('div');
+        const cellArrayWidth = (1/cell_num) * 100;
+        cellArray.style.cssText = `flex-basis: ${cellArrayWidth}%; display: flex; flex-growth: 0; flex-shirnk: 0`;
+        container.appendChild(cellArray);
+
+        cellArray.classList.add('cellArray'); 
+
+        for (let j = 0; j < cells[i].length; j++) {
+            const cell = document.createElement('div');
             
-        });
-        cellArray.appendChild(cell);
-    }    
+            cell.classList.add('cell');  
+            const basis = (1/cell_num) * 100;
+            cell.style.cssText = `flex-basis: ${basis}%; flex-growth: 0; flex-shirnk: 0`;
+            cell.addEventListener('click', (e) => {
+                if (e.target.style.background == 'black') {
+                    e.target.style.background =  `#f9faf8`;
+                }else{
+                    e.target.style.background = 'black';
+                }
+                
+            });
+            cellArray.appendChild(cell);
+        }    
+    }
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
+function DeleteCurrentGrid() {
+    removeAllChildNodes(container);
+    cells = null;
+}
 
-
-
-
-
-
-
-console.log(container)
